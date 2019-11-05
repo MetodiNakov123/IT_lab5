b51ec965a01d7445f148eebcb0d9f0c7c946017d
MetodiNakov123 <nakovmetodi181@gmail.com>
2019-11-05 23:07:04 +0100

    Add Lab5_IT

---
+++ Lab5/Models/AddToRoleModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab5.Models
+{
+    public class AddToRoleModel
+    {
+        public string Email { get; set; }
+
+        public string selectedRole { get; set; }
+
+        public List<string> Roles { get; set; }
+
+        public AddToRoleModel()
+        {
+            Roles = new List<string>();
+        }
+    }
+}
---
+++ Lab5/Models/Friend.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab5.Models
+{
+    public class Friend
+    {
+        [Key]
+        [Required]
+        [Range(0, 200, ErrorMessage = "ID must be between 0 and 200")]
+        [Display(Name = "Friend ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Friend Name")]
+        public string Ime { get; set; }
+
+        [Required]
+        [Display(Name = "Place")]
+        public string MestoZiveenje { get; set; }
+
+    }
+}
